763c4ba467ccb684533284f1ea0862333a10dd7d
Daniel Callaghan <dan_cal89@hotmail.com>
2022-07-26 10:59:57 +0000

    complete func

--- goals/urls.py
+++ goals/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import (
-    goals_list
+    goals_list,
+    update_goal,
+    delete_goal
 )
 
 urlpatterns = [
     path('', goals_list, name="goals_list"),
+    path('update-goal/<int:pk>/', update_goal, name="update_goal"),
+    path('delete-goal/<int:pk>/', delete_goal, name="delete_goal"),
 ]
--- goals/views.py
+++ goals/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Goal
 # Create your views here.
 
-
 def goals_list(request):
-    return render(request, 'goals/func_goals/goals_list.html', {})
+
+    if request.method == 'POST':
+        goal_body = request.POST.get('goal') # check payload and find goal
+        Goal.objects.get_or_create(user=request.user, body=goal_body)[0] # make a goal if it doesn't exist
+        return redirect('goals_list')
+    
+    goals = request.user.goals.all()
+    return render(request, 'goals/func_goals/goals_list.html', {'goals': goals})
+
+def update_goal(request, pk):
+    goal = get_object_or_404(Goal, pk=pk)
+    goal.body = request.POST.get('goal')
+    goal.save()
+    return redirect('goals_list')
+
+def delete_goal(request, pk):
+    goal = get_object_or_404(Goal, pk=pk)
+    goal.delete()
+    return redirect('goals_list')
