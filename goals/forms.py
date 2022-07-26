2bd794eb4ab08c72501cdb4081889c11d20e0603
Daniel Callaghan <dan_cal89@hotmail.com>
2022-07-26 20:46:55 +0000

    forms

---
+++ goals/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Goal
+
+class SimpleGoalForm(forms.Form):
+    goal = forms.CharField(label=False, widget=forms.Textarea(attrs={'class': "form-control"}))
+
+class ModelGoalForm(forms.ModelForm):
+
+    class Meta:
+        model = Goal
+        fields = ('body',)
+        labels = {
+           'body' : False,
+        }
+        # widgets = {
+        #     'body': Textarea(attrs={'class': "form-control"}),
+        # }
--- goals/views.py
+++ goals/views.py
@@ -3,25 +3,35 @@ from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 
 from .models import Goal
+from .forms import SimpleGoalForm, ModelGoalForm
 # Create your views here.
 
 @login_required
 def goals_list(request):
 
     if request.method == 'POST':
-        goal_body = request.POST.get('goal') # check payload and find goal
-        Goal.objects.get_or_create(user=request.user, body=goal_body)[0] # make a goal if it doesn't exist
+        form = ModelGoalForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            form.instance.user = request.user
+            form.save()
+        # goal_body = request.POST.get('goal') # check payload and find goal
+        # Goal.objects.get_or_create(user=request.user, body=goal_body)[0] # make a goal if it doesn't exist
         return redirect('goals_list')
     
     goals = request.user.goals.all()
-    return render(request, 'goals/func_goals/goals_list.html', {'goals': goals})
+    create_form = ModelGoalForm()
+    edit_form = SimpleGoalForm()
+    return render(request, 'goals/func_goals/goals_list.html', {'goals': goals, 'edit_form': edit_form, 'create_form': create_form})
 
 @login_required
 @require_POST
 def update_goal(request, pk):
     goal = get_object_or_404(Goal, pk=pk)
-    if goal.user == request.user:
-        goal.body = request.POST.get('goal')
+    form = SimpleGoalForm(request.POST)
+    
+    if goal.user == request.user and form.is_valid():
+        goal.body = form.cleaned_data["goal"]
         goal.save()
     return redirect('goals_list')
 
