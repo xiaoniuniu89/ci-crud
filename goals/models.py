b6e0b1759d5b92f6dcbbaa0d9e1ae2111a14c840
Daniel Callaghan <dan_cal89@hotmail.com>
2022-07-27 20:28:24 +0100

    end 1st webinar

--- goals/models.py
+++ goals/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Goal(models.Model):
-   user = models.ForeignKey(User, related_name="goals", on_delete=models.CASCADE)
+   user = models.ForeignKey(User, related_name="goals", on_delete=models.CASCADE) # user.goals.all()
    body = models.CharField(max_length=128)
 
    def __str__(self):
--- goals/views.py
+++ goals/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Goal
 # Create your views here.
 
 
 def goals_list(request):
-    return render(request, 'goals/func_goals/goals_list.html', {})
+    goals = request.user.goals.all()
+    
+    if request.method == "POST":
+        goal = request.POST.get('goal')
+        Goal.objects.get_or_create(user=request.user, body=goal)[0]
+        return redirect('goals_list')
+
+
+    return render(request, 'goals/func_goals/goals_list.html', {'goals': goals})
+
+
+def update_goal(request, pk):
+    goal = get_object_or_404(Goal, pk=pk)
+    
+    if request.method == "POST":
+        goal.body = request.POST.get('goal')
+        goal.save()
+        return redirect('goals_list')
+    
+    return render(request, 'goals/func_goals/update_goal.html', {'goal': goal})
+
+
+def delete_goal(request, pk):
+   goal = get_object_or_404(Goal, pk=pk)
+ 
+   if request.method == 'POST':
+       goal.delete()
+       return redirect('goals_list')
+ 
+   return render(request, 'goals/func_goals/delete_goal.html', {'goal': goal})
